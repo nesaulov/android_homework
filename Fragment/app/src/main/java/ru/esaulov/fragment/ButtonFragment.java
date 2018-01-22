241ca63c5daa7f07405a07a68b9be08953b37404
Nikita Esaulov <billikota@gmail.com>
2018-01-22 04:32:27 +0300

    Lab 10

---
+++ Fragment/app/src/main/java/ru/esaulov/fragment/ButtonFragment.java
@@ -0,0 +1,20 @@
+package ru.esaulov.fragment;
+
+/**
+ * Created by nesaulov on 22/01/2018.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ButtonFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.button_fragment, container, false);
+    }
+}
